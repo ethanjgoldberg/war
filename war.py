ea72ae9a4b5cb77e03a4a40fd9709f5884c41043
Ethan J. Goldberg <ethanjgoldberg@gmail.com>
2010-07-24 22:32:51 -0700

    use with statement

--- war.py
+++ war.py
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage:", sys.argv[0], "<outfile>"
         sys.exit()
-    outfile = open(sys.argv[1], 'w')
-    GAME.AddMovable(m.Ship(200 + 200j, 0j, 0, Order, '1'))
-    GAME.AddMovable(m.Ship(350 + 350j, 0j, 0, Order2, '2'))
-    won = False
-    for i in range(500):
-        GAME.Write(outfile)
-        GAME.Move()
-        GAME.Collide()
-        winner = GAME.Win()
-        if winner and not won:
-            outfile.writelines(["> Team ", str(winner), "has won the battle."])
-            won = True
+    with outfile as open(sys.argv[1], 'w'):
+        GAME.AddMovable(m.Ship(200 + 200j, 0j, 0, Order, '1'))
+        GAME.AddMovable(m.Ship(350 + 350j, 0j, 0, Order2, '2'))
+        won = False
+        for i in range(500):
+            GAME.Write(outfile)
+            GAME.Move()
+            GAME.Collide()
+            winner = GAME.Win()
+            if winner and not won:
+                outfile.writelines(["> Team ", str(winner), "has won the battle."])
+                won = True
